639b9620dee1687194284dfc6d753f70ee638e00
Mabia <49458473+prisciladuarte@users.noreply.github.com>
2021-01-14 21:56:48 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,10 @@
+
+lista_frutas = "banana", "laranja", "melancia", "uva"
+for fruta in lista_frutas:
+    print(f"A fruta é {fruta}")
+
+i = 0
+tan = lista_frutas.count(i)
+while 1 <= tan:
+    print(f"A fruta é {lista_frutas[i]}")
+    i += 1
